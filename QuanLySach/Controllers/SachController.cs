031578966716426e9667f4bbfe6b807d3c28e814
pomion <86464757+pomion@users.noreply.github.com>
2021-07-16 09:32:26 +0700

    cmmsu

--- QuanLySach/Controllers/SachController.cs
+++ QuanLySach/Controllers/SachController.cs
@@ -12,8 +12,12 @@ namespace QuanLySach.Controllers
     {
         Sach[] sachs = new Sach[]
         {
-            new Sach { Id = 1, Title = "Tôi Thấy Hoa Vàng Trên Cỏ Xanh", AuthorName = "Nguyễn Nhật Ánh", Price = 1, Content="Truyện Kể Về Tuổi Thơ ..."},
-            new Sach { Id = 2, Title = "Cho Tôi Xin Một Vé Về Tuổi Thơ", AuthorName = "Phan Văn Tuấn", Price = 1, Content = "Truyện Kể Về Tuổi Thơ ..."},
+            new Sach { Id = 1, Title = "Tôi Thấy Hoa Vàng Trên Cỏ Xanh", 
+                AuthorName = "Nguyễn Nhật Ánh", Price = 1, 
+                Content="Truyện Kể Về Tuổi Thơ ..."},
+            new Sach { Id = 2, Title = "Cho Tôi Xin Một Vé Về Tuổi Thơ", 
+                AuthorName = "Phan Văn Tuấn",
+                Content = "Truyện Kể Về Tuổi Thơ ...", Price = 3.75M},
         };
         public IEnumerable<Sach> GetAll()
         {
